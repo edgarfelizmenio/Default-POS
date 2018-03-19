8868f0fbcdd7e3824bea840064c3078b4a104487
Edgar Felizmenio <edgarfelizmenio@gmail.com>
2018-03-20 03:55:42 +0800

    default pos

--- api.py
+++ api.py
@@ -9,22 +9,22 @@ import models
 
 @app.route('/test/save/<int:num_threads>/<int:num_users>/<int:file_size>/<int:policy_size>/<int:num_attributes>')
 def test_save(num_threads, num_users, file_size, policy_size, num_attributes):
-    file_name = 'time_save_encounter_{}_{}.txt'.format(num_users, file_size)
+    file_name = 'time_save_encounter_{}_{}_{}_{}_{}.txt'.format(num_threads, num_users, file_size, policy_size, num_attributes)
     if os.path.exists(os.path.join(models.results_dir, file_name)):
         return send_from_directory(models.results_dir, file_name)
     else:
-        models.save_encounter_test.delay(num_users, file_size)
+        models.save_encounter_test.delay(num_threads, num_users, file_size, policy_size, num_attributes)
         response = {'status': 200, 'message': 'Save test started.'}
         pos_response = jsonify(response)
         return pos_response
 
 @app.route('/test/query/<int:num_threads>/<int:num_users>/<int:file_size>/<int:policy_size>/<int:num_attributes>')
 def test_query(num_threads, num_users, file_size, policy_size, num_attributes):
-    file_name = 'time_query_encounter_{}_{}.txt'.format(num_users, file_size)
+    file_name = 'time_query_encounter_{}_{}_{}_{}_{}.txt'.format(num_users, file_size)
     if os.path.exists(os.path.join(models.results_dir, file_name)):
         return send_from_directory(models.results_dir, file_name)
     else:
-        models.query_encounter_test.delay(num_users, file_size)
+        models.query_encounter_test.delay(num_threads, num_users, file_size, policy_size, num_attributes)
         response = {'status': 200, 'message': 'Query test started.'}
         pos_response = jsonify(response)
         return pos_response
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import os
 import time
 import json
 import requests
+import datetime
 
 from requests.auth import HTTPBasicAuth
 from multiprocessing.pool import ThreadPool
@@ -19,8 +20,8 @@ results_dir = 'results'
 
 encounter_ids_file_name = 'encounter_ids.json'
 encounters_file_name = 'encounters.json'
-save_file_name = os.path.join(results_dir, 'time_save_encounter_{}_{}.txt')
-query_file_name = os.path.join(results_dir, 'time_query_encounter_{}_{}.txt')
+save_file_name = os.path.join(results_dir, 'time_save_encounter_{}_{}_{}_{}_{}.txt')
+query_file_name = os.path.join(results_dir, 'time_query_encounter_{}_{}_{}_{}_{}.txt')
 # DIMENSIONS
 # num_threads, num_users, file_size, policy_size, num_attributes
 
@@ -28,7 +29,7 @@ if not os.path.exists(results_dir):
     os.mkdir(results_dir)
 
 @celery.task()
-def save_encounter_test(num_users, file_size):
+def save_encounter_test(num_threads, num_users, file_size, policy_size, num_attributes):
     with open(test_encounters_file_name.format(file_size), 'r') as encounters_file:
         encounters = json.load(encounters_file)
         
@@ -40,28 +41,34 @@ def save_encounter_test(num_users, file_size):
     # encounters = encounters[:20]
     def save(encounter):
         start = time.time()
-        response = requests.post('https://default-il.cs300ohie.net:5000/encounters/', json=encounter, headers=headers, auth=auth)
+        response = requests.post('{}/encounters/'.format(il_url), json=encounter, headers=headers, auth=auth)
         end = time.time()
+        print(response.status_code)
         transaction_time = end - start
         transaction_times.append(transaction_time)
         encounter_ids.append(response.json()['encounter_id'])
 
     pool = ThreadPool(num_users)
 
+    test_start_date = datetime.datetime.utcnow()
     for encounter in encounters:
         pool.apply_async(save, (encounter,))
 
     pool.close()
     pool.join()
+    test_end_date = datetime.datetime.utcnow()
 
     with open(encounter_ids_file_name, 'w') as encounter_ids_file:
+        print(len(encounter_ids))
         json.dump(encounter_ids, encounter_ids_file)
 
-    with open(save_file_name.format(num_users, file_size), 'w') as transaction_times_file:
+    with open(save_file_name.format(num_threads, num_users, file_size, policy_size, num_attributes), 'w') as transaction_times_file:
         total_time = sum(transaction_times)
         num_transactions = len(transaction_times)
         avg_txn_time = total_time/float(num_transactions)
         rate = 1/avg_txn_time
+        transaction_times_file.write('Test start: {}\n'.format(test_start_date))
+        transaction_times_file.write('Test end: {}\n'.format(test_end_date))
         transaction_times_file.write('Total number of transactions: {}\n'.format(num_transactions))
         transaction_times_file.write('Total time: {}\n'.format(total_time))
         transaction_times_file.write('Average transaction time: {}\n'.format(avg_txn_time))
@@ -70,47 +77,54 @@ def save_encounter_test(num_users, file_size):
         transaction_times_file.writelines(["{}\n".format(txn_time) for txn_time in transaction_times])
 
 @celery.task()
-def query_encounter_test(num_users, file_size):
+def query_encounter_test(num_threads, num_users, file_size, policy_size, num_attributes):
     with open(encounter_ids_file_name, 'r') as encounter_ids_file:
         encounter_ids = json.load(encounter_ids_file)
+    os.remove(encounter_ids_file_name)
     
-    pool = ThreadPool(num_users)
-
     transaction_times = []
     encounters = []
 
     def query(encounter_id):
         start = time.time()
-        response = requests.get('https://default-il.cs300ohie.net:5000/encounters/{}'.format(encounter_id), headers=headers, auth=auth)
+        response = requests.get('{}/encounters/{}'.format(il_url, encounter_id), headers=headers, auth=auth)
         end = time.time()
+        print(response.status_code)
         transaction_time = end - start
         transaction_times.append(transaction_time)
         encounters.append(response.json())
 
+    pool = ThreadPool(num_users)
+    test_start_date = datetime.datetime.utcnow()
     for encounter_id in encounter_ids:
         pool.apply_async(query, (encounter_id,))
 
     pool.close()
     pool.join()
+    test_end_date = datetime.datetime.utcnow()
 
     with open(encounters_file_name, 'w') as encounters_file:
+        print(len(encounters))
         json.dump(encounters, encounters_file)
 
-    with open(query_file_name.format(num_users, file_size), 'w') as transaction_times_file:
+    with open(query_file_name.format(num_threads, num_users, file_size, policy_size, num_attributes), 'w') as transaction_times_file:
         total_time = sum(transaction_times)
         num_transactions = len(transaction_times)
         avg_txn_time = total_time/float(num_transactions)
         rate = 1/avg_txn_time
+        transaction_times_file.write('Test start: {}\n'.format(test_start_date))
+        transaction_times_file.write('Test end: {}\n'.format(test_end_date))
         transaction_times_file.write('Total number of transactions: {}\n'.format(num_transactions))
         transaction_times_file.write('Total time: {}\n'.format(total_time))
         transaction_times_file.write('Average transaction time: {}\n'.format(avg_txn_time))
         transaction_times_file.write('Transactions per second: {}\n'.format(rate))
         transaction_times_file.write('\n')
         transaction_times_file.writelines(["{}\n".format(txn_time) for txn_time in transaction_times])
+    os.remove(encounters_file_name)
 
 if __name__ == '__main__':
     print('Saving Encounters...')
-    save_encounter_test(1,8)
+    save_encounter_test(1,1,8,1,1)
     print('Querying Encounters...')
-    query_encounter_test(1,8)
+    query_encounter_test(1,1,8,1,1)
     print('Done.')
